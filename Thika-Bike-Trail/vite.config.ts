a5100aea79e425cf5b8bb1e4d5cd47c8969c3de2
Issa Nyale <me@email.com>
2025-04-21 10:06:39 +0300

    Added project Icons

--- Thika-Bike-Trail/vite.config.ts
+++ Thika-Bike-Trail/vite.config.ts
@@ -1,22 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig(({
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  // plugins: [
+  //   react(),
+  //   mode === 'development' &&
+  //   componentTagger(),
+  // ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/icon-192x192.png', 'icons/icon-512x512.png'],
+      manifest: {
+        name: 'Thika Bikes Trail',
+        short_name: 'BikeTrail',
+        description: 'Explore the beautiful biking trails of Thika with our app!',
+        theme_color: '#2e4a3d',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 }));
